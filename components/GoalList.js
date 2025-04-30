1f4546dbed381c3a13de6cf70599b17d46d424fe
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-04-30 02:30:08 -0400

    Created a goal list to display goals for the selected data and a goalContainer that shows what percentage of goals were acheived

---
+++ components/GoalList.js
@@ -0,0 +1,133 @@
+  import { View, Text, FlatList, StyleSheet, Pressable } from "react-native";
+  import { useState } from "react";
+  import { FontAwesome6 } from "@expo/vector-icons";
+  import colors from "../colors";
+  const GoalList = () => {
+    const [goals, setGoals] = useState([
+      {
+        title: "Hack4Impact Interview",
+        description: "Complete Interview for hack4impact",
+        time: new Date(2025, 3, 29, 19, 30),
+        completed: false,
+        id: Math.random() * new Date(2025, 3, 29, 19, 30),
+      },
+      {
+        title: "Call Michael",
+        completed: true,
+        time: Date.now(),
+        id: Math.random() * Date.now(),
+      },
+      {
+        title: "Call Michael",
+        completed: false,
+        time: Date.now(),
+        id: Math.random() * Date.now(),
+      },
+    ]);
+    const toggleTask = (id) => {
+      const updatedGoals = goals.map((goal) =>
+        goal.id === id ? { ...goal, completed: !goal.completed } : goal
+      );
+      setGoals(updatedGoals);
+    };
+    return (
+      <View>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerContainerLeft}>Your Goals & Reminders</Text>
+          <View style={styles.headerContainerRight}>
+            <Text style={styles.headerContainerRightText}>Today</Text>
+            <FontAwesome6
+              name="chevron-down"
+              color={colors.accent}
+              size={15}
+            />
+          </View>
+        </View>
+        <FlatList
+          style={styles.listContainer}
+          data={goals}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+          renderItem={({ item }) => (
+            <View style={[styles.listItem, {borderColor:!item.completed?'rgba(0,0,0,0.1)':'rgba(0,0,0,0.05)'}]}>
+              <View style={styles.listItemLeft}>
+                {!item.completed?<Pressable onPress={()=>toggleTask(item.id)} style={styles.selector} />:<Pressable onPress={()=>toggleTask(item.id)}><FontAwesome6 name="circle-check" color={colors.secondaryText} size={20}/></Pressable>}
+              </View>
+              <View style={styles.listItemRight}>
+                <View style={styles.text}>
+                  <Text style={[styles.title,{color:!item.completed?colors.text:colors.secondaryText}]}>{item.title}</Text>
+                  {item.description && <Text numberOfLines={1} style={[styles.description,{color:!item.completed?colors.secondaryText:'#ADD8B9'}]}>{item.description}</Text>}
+                </View>
+                <FontAwesome6 size={17.5} color={'rgba(0,0,0,0.5)'} name="ellipsis"></FontAwesome6>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    );
+  };
+  const styles = StyleSheet.create({
+    headerContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      borderBottomWidth: 1,
+      borderColor: "rgba(0,0,0,0.35)",
+      paddingBottom: 5,
+      marginBottom:30
+    },
+    headerContainerLeft: {
+      color: colors.accent,
+      fontSize: 14,
+      fontWeight: 500,
+    },
+    headerContainerRight: {
+      fontWeight: 500,
+      alignItems: "center",
+      flexDirection: "row",
+      columnGap: 5,
+    },
+    headerContainerRightText: {
+      fontSize: 14,
+      color: colors.accent,
+      fontWeight: 500,
+    },
+    listItem: {
+      flexDirection: "row",
+      width: '100%',
+      height: "auto",
+      backgroundColor: colors.background,
+      borderWidth:1,
+      padding:15,
+      borderRadius:10,
+      columnGap:15
+    },
+    listItemLeft:{
+      height:'100%',
+      justifyContent:'flex-start'
+    },
+    selector:{
+      width:20,
+      height:20,
+      borderColor:'rgba(0,0,0,0.2)',
+      borderWidth:1.5,
+      borderRadius:30
+    },
+    listItemRight:{
+      flex:1,
+      flexDirection:"row",
+      alignItems:'center',
+      justifyContent:'space-between'
+    },
+    text:{
+      rowGap:3
+    },
+    title:{
+      fontWeight:600,
+      fontSize:14
+    },
+    description:{
+      fontWeight:400,
+    }
+  });
+  export default GoalList;
---
+++ components/GoalsCompleted.js
@@ -0,0 +1,44 @@
+import { View, Text, StyleSheet } from "react-native";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+import colors from "../colors";
+const GoalsCompleted=()=>{
+  
+  return(
+    <View style={styles.goalsContainer}>
+          <View style={styles.goalsText}>
+            <Text style={styles.goalsTextHeader}>Today's goals</Text>
+            <Text style={styles.goalsTextSubheader}>5 out of 10 goals completed</Text>
+          </View>
+          <AnimatedCircularProgress rotation={0} size={75} width={10} fill={(5/10)*100} tintColor="white" backgroundColor={colors.accent}/>
+      </View>
+  )
+}
+const styles = StyleSheet.create({
+  goalsContainer:{
+    
+    height:'auto', 
+    backgroundColor:colors.secondary,
+    justifyContent:'space-between',
+    flexDirection:'row',
+    alignItems:"center",
+    padding:20,
+    paddingTop:25,
+    paddingBottom:25,
+    borderRadius:15,
+ 
+  },
+  goalsText:{
+    rowGap:5
+  },
+  goalsTextHeader:{
+    color:"white",
+    fontWeight:600,
+    fontSize:20
+  },
+  goalsTextSubheader:{
+    color:"white",
+    fontWeight:500,
+    fontSize:14
+  },
+})
+export default GoalsCompleted;
