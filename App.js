79e4fff20dc05f02b99713af70dfe011902f3cb4
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-05-03 21:57:48 -0400

    Upgraded to expo version 53.0

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import TodoDetailScreen from './screens/TodoDetailScreen';
 import CreateTodoScreen from './screens/CreateTodoScreen';
+import { useState } from 'react';
+import { MMKV } from 'react-native-mmkv';
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [todos, setTodos] = useState();
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}}>
