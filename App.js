91e03f2b8c50e9eccee1f9e28d1a14dd20015384
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-05-05 22:43:41 -0400

    Clean up and goalcontext implementation

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import TodoDetailScreen from "./screens/TodoDetailScreen";
 import CreateTodoScreen from "./screens/CreateTodoScreen";
 import { useState } from "react";
-import { MMKV } from "react-native-mmkv";
 import { Provider as PaperProvider } from "react-native-paper";
-import { LocalizationProvider } from "react-native-paper-dates";
+import { GoalProvider } from "./GoalContext";
 const Stack = createNativeStackNavigator();
 export default function App() {
-  const [todos, setTodos] = useState();
   return (
     <PaperProvider>
-      <LocalizationProvider>
+      <GoalProvider>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Home" component={HomeScreen} />
@@ -22,7 +18,7 @@ export default function App() {
             <Stack.Screen name="Details" component={TodoDetailScreen} />
           </Stack.Navigator>
         </NavigationContainer>
-      </LocalizationProvider>
+      </GoalProvider>
     </PaperProvider>
   );
 }
