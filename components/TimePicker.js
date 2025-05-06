993b80f65b953d708916ed5630b66db4c9f34c2e
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-05-06 02:54:34 -0400

    Created a time selector to select time for goals

---
+++ components/TimePicker.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+const TimePicker = ({ time, setTime }) => {
+  return (
+    <View style={styles.pickerContainer}>
+      <Picker
+        style={{ flex: 1 }}
+        selectedValue={time.hours}
+        onValueChange={(ItemValue) =>
+          setTime((prev) => ({ ...prev, hours: ItemValue }))
+        }
+      >
+        <Picker.Item label="1" value={"1"}></Picker.Item>
+        <Picker.Item label="2" value={"2"}></Picker.Item>
+        <Picker.Item label="3" value={"3"}></Picker.Item>
+        <Picker.Item label="4" value={"4"}></Picker.Item>
+        <Picker.Item label="5" value={"5"}></Picker.Item>
+        <Picker.Item label="6" value={"6"}></Picker.Item>
+        <Picker.Item label="7" value={"7"}></Picker.Item>
+        <Picker.Item label="8" value={"8"}></Picker.Item>
+        <Picker.Item label="9" value={"9"}></Picker.Item>
+        <Picker.Item label="10" value={"10"}></Picker.Item>
+        <Picker.Item label="11" value={"11"}></Picker.Item>
+        <Picker.Item label="12" value={"12"}></Picker.Item>
+      </Picker>
+      <Picker
+        style={{ flex: 1 }}
+        selectedValue={time.minutes}
+        onValueChange={(ItemValue) =>
+          setTime((prev) => ({ ...prev, minutes: ItemValue }))
+        }
+      >
+        <Picker.Item label="00" value={"0"}></Picker.Item>
+        <Picker.Item label="05" value={"5"}></Picker.Item>
+        <Picker.Item label="10" value={"10"}></Picker.Item>
+        <Picker.Item label="15" value={"15"}></Picker.Item>
+        <Picker.Item label="20" value={"20"}></Picker.Item>
+        <Picker.Item label="25" value={"25"}></Picker.Item>
+        <Picker.Item label="30" value={"30"}></Picker.Item>
+        <Picker.Item label="35" value={"35"}></Picker.Item>
+        <Picker.Item label="40" value={"40"}></Picker.Item>
+        <Picker.Item label="45" value={"45"}></Picker.Item>
+        <Picker.Item label="50" value={"50"}></Picker.Item>
+        <Picker.Item label="55" value={"55"}></Picker.Item>
+      </Picker>
+      <Picker style={{flex:1}} selectedValue={time.AMPM} onValueChange={(ItemValue)=>setTime((prev)=>({...prev, AMPM:ItemValue}))}>
+        <Picker.Item label="AM" value={"AM"}></Picker.Item>
+        <Picker.Item label="PM" value={"PM"}></Picker.Item>
+      </Picker>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  pickerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderTopColor:"rgba(0,0,0,0.1)",
+    borderTopWidth:1,
+    marginTop:10
+  },
+});
+export default TimePicker;
