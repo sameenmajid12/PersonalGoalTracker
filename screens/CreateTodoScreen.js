788615b3609c2296d32272a60bb573a3a7a9a654
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-05-06 02:55:16 -0400

    Added ScrollView

--- screens/CreateTodoScreen.js
+++ screens/CreateTodoScreen.js
@@ -1,11 +1,6 @@
 import { useContext, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {
-  StyleSheet,
-  View,
-  Text,
-  Pressable,
-} from "react-native";
+import { StyleSheet, View, Text, Pressable, ScrollView } from "react-native";
 import colors from "../colors";
 import CreateTodoInputs from "../components/CreateTodoInputs";
 import { GoalContext } from "../GoalContext";
@@ -13,10 +8,15 @@ const CreateTodoScreen = () => {
   const [type, setType] = useState("goal");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [time, setTime] = useState({ enabled: false, data: null });
+  const [time, setTime] = useState({
+    enabled: false,
+    hours: null,
+    minutes: null,
+    AMPM: "",
+  });
   const [date, setDate] = useState({ enabled: false, data: null });
   const [repeat, setRepeat] = useState({ enabled: false, data: null });
-  const {addGoal} = useContext(GoalContext)
+  const { addGoal } = useContext(GoalContext);
   const toggleOption = (setter) => {
     setter((prev) => ({ ...prev, enabled: !prev.enabled }));
   };
@@ -27,14 +27,14 @@ const CreateTodoScreen = () => {
     setType(t);
   };
   const save = () => {
-    if(!title.trim()) return;
+    if (!title.trim()) return;
     const id = Math.random().toString(36).substring(2, 9);
     const goal = {
       id: id,
       title: title,
       description: description,
       time: time.data,
-      date: date.data,
+      date: date.data ? date.data : new Date(),
       repeat: repeat.data,
       type: type,
     };
@@ -48,76 +48,78 @@ const CreateTodoScreen = () => {
   };
   return (
     <SafeAreaView style={styles.createScreen}>
-      <View style={styles.createHeaderContainer}>
-        <Text style={styles.createHeader}>Create new</Text>
-        <View style={styles.createButtonsContainer}>
-          <Pressable
-            onPress={() => changeType("goal")}
-            style={[
-              styles.createButton,
-              type === "goal"
-                ? { backgroundColor: colors.accent }
-                : {
-                    backgroundColor: "transparent",
-                    borderWidth: 1,
-                    borderColor: colors.accent,
-                  },
-            ]}
-          >
-            <Text
-              style={
+      <ScrollView  style={styles.scrollContainer}>
+        <View style={styles.createHeaderContainer}>
+          <Text style={styles.createHeader}>Create new</Text>
+          <View style={styles.createButtonsContainer}>
+            <Pressable
+              onPress={() => changeType("goal")}
+              style={[
+                styles.createButton,
                 type === "goal"
-                  ? { color: "white", fontWeight: "500" }
-                  : { color: colors.accent, fontWeight: "400" }
-              }
+                  ? { backgroundColor: colors.accent }
+                  : {
+                      backgroundColor: "transparent",
+                      borderWidth: 1,
+                      borderColor: colors.accent,
+                    },
+              ]}
             >
-              Goal
-            </Text>
-          </Pressable>
-          <Pressable
-            onPress={() => changeType("reminder")}
-            style={[
-              styles.createButton,
-              type === "reminder"
-                ? { backgroundColor: colors.accent }
-                : {
-                    backgroundColor: "transparent",
-                    borderWidth: 1,
-                    borderColor: colors.accent,
-                  },
-            ]}
-          >
-            <Text
-              style={
+              <Text
+                style={
+                  type === "goal"
+                    ? { color: "white", fontWeight: "500" }
+                    : { color: colors.accent, fontWeight: "400" }
+                }
+              >
+                Goal
+              </Text>
+            </Pressable>
+            <Pressable
+              onPress={() => changeType("reminder")}
+              style={[
+                styles.createButton,
                 type === "reminder"
-                  ? { color: "white", fontWeight: "500" }
-                  : { color: colors.accent, fontWeight: "400" }
-              }
+                  ? { backgroundColor: colors.accent }
+                  : {
+                      backgroundColor: "transparent",
+                      borderWidth: 1,
+                      borderColor: colors.accent,
+                    },
+              ]}
             >
-              Reminder
-            </Text>
+              <Text
+                style={
+                  type === "reminder"
+                    ? { color: "white", fontWeight: "500" }
+                    : { color: colors.accent, fontWeight: "400" }
+                }
+              >
+                Reminder
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.break} />
+        <CreateTodoInputs
+          time={time}
+          setTime={setTime}
+          date={date}
+          setDate={setDate}
+          repeat={repeat}
+          setRepeat={setRepeat}
+          toggleOption={toggleOption}
+          title={title}
+          setTitle={setTitle}
+          setDescription={setDescription}
+          description={description}
+        />
+        <View style={styles.saveButtonContainer}>
+          <Pressable onPress={save} style={styles.saveButton}>
+            <Text style={styles.saveButtonText}>Save</Text>
           </Pressable>
         </View>
-      </View>
-      <View style={styles.break} />
-      <CreateTodoInputs
-        time={time}
-        setTime={setTime}
-        date={date}
-        setDate={setDate}
-        repeat={repeat}
-        setRepeat={setRepeat}
-        toggleOption={toggleOption}
-        title={title}
-        setTitle={setTitle}
-        setDescription={setDescription}
-        description={description}
-      />
-      <View style={styles.saveButtonContainer}>
-        <Pressable onPress={save} style={styles.saveButton}>
-          <Text style={styles.saveButtonText}>Save</Text>
-        </Pressable>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -125,8 +127,10 @@ const styles = StyleSheet.create({
   createScreen: {
     backgroundColor: colors.background,
     flex: 1,
-    padding: 20,
-    rowGap: 20,
+  },
+  scrollContainer:{
+    flex:1,
+    padding:20,
   },
   createHeaderContainer: {
     rowGap: 20,
@@ -148,6 +152,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   break: {
+    marginVertical:20,
     borderBottomWidth: 1,
     borderBottomColor: "rgba(0,0,0,0.25)",
   },
@@ -162,6 +167,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: colors.accent,
     borderRadius: 5,
+    marginTop:20
   },
   saveButtonText: {
     color: "white",
